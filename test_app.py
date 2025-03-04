17427623a3eff720ddbee817c39619f3f78a4bc8
gmasta-office <mgmkz90@gmail.com>
2025-03-04 21:26:47 +0500

    Another Commit

--- test_app.py
+++ test_app.py
@@ -3,9 +3,7 @@ from app import app
 
 @pytest.fixture
 def client():
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
+    return app.test_client()
 
 def test_home_page(client):
     response = client.get("/")
